ae881b5474a668c895ff543333fcc8ae737e0327
HVmech <130397692+HVmech@users.noreply.github.com>
2023-10-02 09:26:19 +0300

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -4,8 +4,10 @@
 #include <ctime>
 #include "headers.h"
 
-int game(int maxnum, std::string& usrname, bool flg){
-    if(flg) {
+int game(int maxnum, std::string& usrname, bool flg)
+{
+    if(flg)
+    {
         // Блок знакомства с пользователем
         std::cout << "Welcome to 'Guess the number' game!" << std::endl;
         std::cout << "Enter your username:" << std::endl;
@@ -14,53 +16,70 @@ int game(int maxnum, std::string& usrname, bool flg){
         std::srand(static_cast<unsigned>(std::time(nullptr)));
         std::cout << "Ok, let's go! :)" << std::endl;
     }
-
-	std::cout << "Guess the number from 1 up to " << maxnum << ":" << std::endl;
-	int targnum = std::rand() % maxnum + 1;
-	int attnum = 0;
+	
+    std::cout << "Guess the number from 1 up to " << maxnum << ":" << std::endl;
+    int targnum = std::rand() % maxnum + 1;
+    int attnum = 0;
     int usrnum;
 
-	do {
-		std::cin >> usrnum;
-			++attnum;
+    do
+    {
+	std::cin >> usrnum;
+	++attnum;
 
-			if (usrnum > targnum) {
-				std::cout << "It's less than " << usrnum << std::endl;
-			}
-			else if (usrnum < targnum) {
-				std::cout << "It's greater than " << usrnum << std::endl;
-			}
-			else{
-				std::cout << "Congratulations, " << usrname << "! You win! It's " << targnum << "!" << std::endl;
-				break;
-			}
-        if(attnum > maxnum) return -1;
-    } while(true);
-
+	if (usrnum > targnum)
+	{
+	     std::cout << "It's less than " << usrnum << std::endl;
+	}
+	else if (usrnum < targnum)
+	{
+	     std::cout << "It's greater than " << usrnum << std::endl;
+	}
+	else
+	{
+	     std::cout << "Congratulations, " << usrname << "! You win! It's " << targnum << "!" << std::endl;
+	     break;
+	}
+        
+	if(attnum > maxnum) return -1;
+    }
+    while(true);
+	
     return attnum;
 }
 
-int restart_offer(){
+int restart_offer()
+{
     std::string responce;
 
     // Блок предложения повторной игры
-	for (int i=1; ; ++i){
-		std::cout << "Would you like to play again? [y/n]:" << std::endl;
-		std::cin >> responce;
+    for (int i=1; ; ++i)
+    {
+        std::cout << "Would you like to play again? [y/n]:" << std::endl;
+	std::cin >> responce;
 
-		if (responce == "y"){
-			std::cout << "Good :)" << std::endl << "Let's start!" << std::endl;
-			return 1;
-		}
-		if (responce == "n"){
-			std::cout << "Ok :(" << std::endl << "C u!" << std::endl;
-		    return 0;
-		}
-		if (i > 2){
-			std::cout << "We can't understand each other. Bye!" << std::endl;
-			return -1;
-		} else{
-			std::cout << "I can't understand your answer. Please, enter correct one..." << std::endl;
-		}
+	if (responce == "y")
+	{
+	    std::cout << "Good :)" << std::endl << "Let's start!" << std::endl;
+		
+	    return 1;
 	}
+		
+	if (responce == "n")
+	{
+	    std::cout << "Ok :(" << std::endl << "C u!" << std::endl;
+		    
+	    return 0;
+	}
+		
+	if (i > 2)
+	{
+	    std::cout << "We can't understand each other. Bye!" << std::endl;
+	    return -1;
+	}
+	else
+	{
+	    std::cout << "I can't understand your answer. Please, enter correct one..." << std::endl;
+	}
+    }
 }
